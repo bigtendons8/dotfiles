fd57b9359df271266ed76ef57bc69f6211899ea6
bigtendons8 <154099045+bigtendons8@users.noreply.github.com>
2024-07-29 10:32:02 +1000

    remove nvim because my config is boring

--- nvim/lua/james/plugins/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "scottmckendry/cyberdream.nvim",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme cyberdream")
-    vim.cmd('hi Normal ctermbg=NONE guibg=NONE')
-    vim.cmd('hi NonText ctermbg=NONE guibg=NONE')
-  end
-}
